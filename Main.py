4ea50c5fe434a3b4e4abd190eee8eaaeef6ebc9c
tml27 <ugactye12@gmail.com>
2025-12-08 01:45:47 +0700

    Better appearance

--- Main.py
+++ Main.py
@@ -1,21 +1,14 @@
 import pygame
 import numpy as np
+import time
 from Cloth import cloth_t
-
-SCREEN_RES = (1600, 900)
-FPS = 60
-FDELTA_T = 1.0 / 60.0
+from config import *
 
 pygame.init()
-screen = pygame.display.set_mode(SCREEN_RES)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-ROWS, COLS = 50, 50
-SPACING = 10
-STIFFNESS = 0.5
-SCREEN_OFFSET = (SCREEN_RES[0] - (COLS - 1) * SPACING) / 2
-
-my_cloth = cloth_t(ROWS, COLS, SPACING, STIFFNESS, SCREEN_OFFSET)
+my_cloth = cloth_t()
 
 start_pos = np.array([0, 0])
 end_pos = np.array([0, 0])
@@ -23,9 +16,11 @@ is_cutting = False
 is_running = True
 drag_point_idx = None
 
-prev_mouse_pos = np.array([0.0, 0.0])
+trail = []
+prev_mouse_pos = None
 
 while is_running:
+    cur_time = time.time()
     clock.tick(FPS)
     fps_now = clock.get_fps()
     pygame.display.set_caption(f"Cloth Simulation by lmToT27 - FPS: {fps_now:.2f}")
@@ -38,7 +33,7 @@ while is_running:
             continue
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
-                my_cloth = cloth_t(ROWS, COLS, SPACING, STIFFNESS, SCREEN_OFFSET)
+                my_cloth = cloth_t()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 is_cutting = True
@@ -77,22 +72,32 @@ while is_running:
             my_cloth.pos[drag_point_idx] = cur_mouse_pos.copy()
             my_cloth.old_pos[drag_point_idx] = cur_mouse_pos.copy()
     
-    my_cloth.UpdatePoints(FDELTA_T)
-    my_cloth.SolveConstraints(5)
-    prev_mouse_pos = cur_mouse_pos
+    my_cloth.UpdatePoints()
+    my_cloth.SolveConstraints()
 
     screen.fill("#000000")
-    
+
+    if is_cutting:
+        cur_mouse_pos = pygame.mouse.get_pos()
+        if prev_mouse_pos is not None:
+            trail.append((prev_mouse_pos, cur_mouse_pos, cur_time))
+        prev_mouse_pos = cur_mouse_pos
+    new_trail = []
+
+    for p1, p2, t in trail:
+        age = cur_time - t
+        if age <= TRAIL_DURATION:
+            pygame.draw.line(screen, "#89FFFD", p1, p2, 1)
+            new_trail.append((p1, p2, t))
+    trail = new_trail
+
     if len(my_cloth.lines) > 0:
         start_points = my_cloth.pos[my_cloth.lines[:, 0]]
         end_points = my_cloth.pos[my_cloth.lines[:, 1]]
-
-        if is_cutting: pygame.draw.line(screen, (50, 255, 215), start_pos, end_pos, 2)
         
-        for pos in my_cloth.pos:
-            pygame.draw.circle(screen, (250, 145, 255), (pos[0], pos[1]), 2)
         for i in range(len(start_points)):
-            pygame.draw.line(screen, (250, 200, 255), start_points[i], end_points[i], 1)
+            pygame.draw.line(screen, "#FFA5F0", start_points[i], end_points[i], 1)
+    prev_mouse_pos = cur_mouse_pos
 
     pygame.display.update()
 
